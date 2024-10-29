bac50a7490f04db7e46bb283d2083d7e98963001
Jacques Coertze <jacques.coertze@gmail.com>
2024-10-29 07:14:36 +0200

    init: Initial TanStack Start Template

---
+++ .eslintrc.cjs
@@ -0,0 +1,46 @@
+/** @type {import("eslint").Linter.Config} */
+const config = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: true,
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:@typescript-eslint/stylistic-type-checked',
+    'plugin:eslint-comments/recommended',
+    'plugin:promise/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react/function-component-definition': 'off',
+    'react/jsx-props-no-spreading': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/array-type': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/consistent-type-imports': [
+      'warn',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    '@typescript-eslint/require-await': 'off',
+    '@typescript-eslint/no-misused-promises': [
+      'error',
+      {
+        checksVoidReturn: {
+          attributes: false,
+        },
+      },
+    ],
+  },
+};
+module.exports = config;
---
+++ prettier.config.mjs
@@ -0,0 +1,12 @@
+/**
+ * @type {import('prettier').Config & import("@ianvs/prettier-plugin-sort-imports").PluginConfig}
+ */
+const config = {
+  printWidth: 100,
+  singleQuote: true,
+  semi: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+};
+
+export default config;
